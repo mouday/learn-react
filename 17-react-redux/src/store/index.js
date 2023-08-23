0a791c53d4d78fe8c426ede8fd9cdd85394507b4
pengshiyu <1940607002@qq.com>
2023-08-23 09:38:14 +0800

    fix

--- 17-react-redux/src/store/index.js
+++ 17-react-redux/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
-import { reducers } from "./reducers.js";
+import { counter } from "./reducers.js";
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const store = createStore(counter, applyMiddleware(thunkMiddleware));
 
 export default store;
